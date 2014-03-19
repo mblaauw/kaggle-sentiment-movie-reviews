8775380ff8a35464d0e2da35f3d480f41cff6edd
michel.blaauw@gmail.com <michel.blaauw@gmail.com>
2014-03-19 23:58:34 +0100

    ..

--- calculate_sentiment.r
+++ calculate_sentiment.r
@@ -1,19 +1,19 @@
 library(plyr)
+library("klaR")
+library("caret")
+library(e1071) 
 
-test <- read.delim("~/Downloads/04_R_PROJECTS/kaggle-sentiment-movie-reviews/data/test.tsv")
-train <- read.delim("~/Downloads/04_R_PROJECTS/kaggle-sentiment-movie-reviews/data/train.tsv")
+testset <- read.delim("./data/test.tsv")
+trainset <- read.delim("./data/train.tsv")
 
-
+# -------------------------------
 train$Phrase = tolower(train$Phrase)
-
 NegTerms <- train$Phrase[train$Sentiment==0 | train$Sentiment==1]
 NeutralTerms <- train$Phrase[train$Sentiment==2]
 PosTerms <- train$Phrase[train$Sentiment==3 | train$Sentiment==4]
 
-
 #function to calculate number of words in each category within a sentence
-sentimentScore <- function(sentences, NegTerms, NeutralTerms, PosTerms){
-  
+sentimentScore <- function(sentences, NegTerms, NeutralTerms, PosTerms){  
   
   final_scores <- matrix('', 0, 5)
   
@@ -46,6 +46,26 @@ sentimentScore <- function(sentences, NegTerms, NeutralTerms, PosTerms){
   }, NegMatches, NeutralMatches, PosMatches)
   return(scores)
 }    
+# -------------------------------
+
+
+
+train_sample = trainset[3:4]
+
+x = train_sample$Phrase
+y = train_sample$Sentiment
+
+x_sample = x[1:20]
+y_sample = y[1:20]
+
+model = train(x,y, tuneLength=1)
+
+
+#--e1071
+classifier <- naiveBayes(x, y) 
+predict(classifier, x)
+confTable <- table(predict(classifier, results), results[,6], dnn=list('predicted','actual'))
+confTable  
 
 
 #build tables of positive and negative sentences with scores
